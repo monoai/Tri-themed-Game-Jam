6a931a4dfefedfa615b04f2bc1a99c1a1c1ec01c
monoai <monoai@users.noreply.github.com>
2020-06-12 02:40:17 +0800

    Added nice comments
    I'm pretty nice HAHAHAHAHA, dejk I need those comments for me in the morning.

--- Fortress.gd
+++ Fortress.gd
@@ -2,23 +2,21 @@ extends Node2D
 
 export(PackedScene) var Soldier
 
-var v
-
 func _ready():
 	pass # Replace with function body.
 
-func get_towerpos(towerPos): #Rename this function if it's gonna be the clicking mechanic
-	print("Tower position:" + String(towerPos))
+func get_towerpos(towerPos): #Rename this function if it's gonna be the clicking mechanic, unless tamad tayo lmao
+	print("Tower position: " + String(towerPos)) #Debug for looking positions
 	var soldier = Soldier.instance()
 	soldier.position = position
 	add_child(soldier)
 	soldier.position = Vector2(0,0)
-	v = Vector2(towerPos - position).normalized() * 1000
+	var v = Vector2(towerPos - position).normalized() * 1000 #Code was only based from the bullets code we had last time, we can probably modify this better.
 	soldier.linear_velocity = v
 	soldier.rotation = atan2(v.y,v.x)
 
 func create_soldier(): #Supposedly spawn the soldiers but somehow i found another way instead
 	pass
 
-func _on_ShootTimer_timeout():
+func _on_ShootTimer_timeout(): #I guess we could use this if we're not gonna let the fortress shoot an infinite and fast amount of soldiers
 	pass # Replace with function body.
